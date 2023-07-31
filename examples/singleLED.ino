216b43d2d3c9d3e2b5002204cf2bd1364d562195
Yuna Iwata <115478706+yuna-iwata@users.noreply.github.com>
2023-07-31 23:37:00 +0100

    add example single LED

---
+++ examples/singleLED.ino
@@ -0,0 +1,23 @@
+//install FastLED by daniel garcia from lib manager
+//this will light the first LED with the colour (255,0,0)
+#include <FastLED.h>
+#define LED_PIN 4
+#define NUM_LEDS 100
+
+CRGB leds[NUM_LEDS];
+
+
+void setup() {
+  // put your setup code here, to run once:
+  FastLED.addLeds<WS2812, LED_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5,500);
+  FastLED.clear();
+  FastLED.show();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  leds[0] = CRGB(225,0,0);
+  FastLED.show();
+}
